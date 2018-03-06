1a8c985e88bb9949d679238c18e51b3f6a8de0e4
Yyyyaaaannnnoooo <yannpatrickmartins@gmail.com>
2018-03-06 15:35:54 +0100

    add colorful animation

--- js/sketch.js
+++ js/sketch.js
@@ -1,7 +1,33 @@
+let minBallSize = 50;
+let maxBallSize = 200;
+let ellipseSize = 0;
+let colorVal = 0;
+const rectW = minBallSize;
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+	let cnv = createCanvas(windowWidth, windowHeight);
+	cnv.position(0, 0);
+	cnv.style('z-index', '-1');
+	cnv.style('position', 'fixed');
+	document.getElementById('defaultCanvas0').style.position = 'fixed';
+	colorz = [
+		color(255, 0, 0),
+		color(0, 255, 0),
+		color(0, 0, 255),
+		color(0, 255, 255),
+	];
+	colorMode(HSB);
 }
 
 function draw() {
-
+	colorVal = map(mouseX, 0, width, 0, 255);
+	let angle = map(mouseX, 0, width, 0, PI);
+	ellipseSize = abs(cos(angle) * maxBallSize) + minBallSize;
+	background(colorVal, 255, 255)
+	noStroke();
+	fill(mouseX % 255, 255, 255);
+	rect(width/ 2 - rectW / 2, 0, rectW, mouseY - minBallSize / 2);
+	rect(width/ 2 - rectW / 2, (mouseY - minBallSize / 2) + minBallSize, rectW, height);
+	fill(255 - colorVal, 50, 200);
+	ellipse(mouseX, mouseY, ellipseSize);
 }
+
