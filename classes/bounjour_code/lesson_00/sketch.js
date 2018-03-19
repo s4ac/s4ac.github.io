d06f4a42e384856624560368c3732e9bc5e160ba
Yyyyaaaannnnoooo <yannpatrickmartins@gmail.com>
2018-03-19 16:16:19 +0100

    add directory lesson_00 with empty sketch

---
+++ classes/bounjour_code/lesson_00/sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+
+}
+// responsive design
+function windowResized(){
+	resizeCanvas(windowWidth, windowHeight);
+	// here you should add code to scale the dimension of your elements in the canvas
+}
