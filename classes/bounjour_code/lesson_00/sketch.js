52b8356c06fc5854dd33518ac258a68eff818800
Yyyyaaaannnnoooo <yannpatrickmartins@gmail.com>
2018-03-19 21:00:09 +0100

    add lesson_00

--- classes/bounjour_code/lesson_00/sketch.js
+++ classes/bounjour_code/lesson_00/sketch.js
@@ -1,12 +1,33 @@
+//GLOBAL VARIABLES
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+  // put setup code here
+  createCanvas(innerWidth, innerHeight);
+  fill(255, 0, 0);
+  //TO PRINT SOMETHING TO THE VONSOLE USE
+  console.log('a random number: ' + random(100));
+  // ellipse(100, 145, 50, 100);//draws a circle
 }
 
 function draw() {
-
+  // put drawing code here
+  background(150);// draws a colored or b/w background
+  line(width / 2, 0, width / 2, height);//draws a line(x1, y1, x2, y2)
+  line(0, height / 2, width, height / 2);
+  // if statements
+  if (mouseX < width / 2 && mouseY < height / 2) {
+    fill(255, 0, 0);//fills the form with a color r, g, b
+  } else if (mouseX > width / 2 && mouseY < height / 2) {
+    fill(0, 255, 0);
+  } else if (mouseX > width / 2 && mouseY > height / 2) {
+    fill(0, 0, 255);
+  } else if (mouseX < width / 2 && mouseY > height / 2) {
+    fill(0, 255, 255);
+  }
+  rect(mouseX, mouseY, 200, 300);//draws a rectangle
 }
-// responsive design
-function windowResized(){
-	resizeCanvas(windowWidth, windowHeight);
-	// here you should add code to scale the dimension of your elements in the canvas
+/**
+ * this function makes your canvas responsive
+ */
+function windowResized() {
+  resizeCanvas(innerWidth, innerHeight);
 }
