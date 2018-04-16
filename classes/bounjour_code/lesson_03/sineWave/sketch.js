6a8994ac603c4c9adab8717e7e06f9b691f274c0
Yyyyaaaannnnoooo <yannpatrickmartins@gmail.com>
2018-04-16 10:34:16 +0200

    add comments

--- classes/bounjour_code/lesson_03/sineWave/sketch.js
+++ classes/bounjour_code/lesson_03/sineWave/sketch.js
@@ -1,17 +1,17 @@
-let radius = 30;
+let radius = 10;
 let vertices = 50;
-let loop = 20;
+let loop = 20;//number of polygons
 let wave = 0;
-let waveHeight = 5;
-let waveFreq = 1;
-let period = 2;
-let inc;
+let waveHeight = 2;// height of the wave
+let waveFreq = 1;// frequency of the wave
+let period = 2;// how many time the waveFreq cycles trought two_PI
+let inc;// this regulates by how much the sinWave growsa aka speed
 let slider;
 let cnv;
 function setup() {
   cnv = createCanvas(innerWidth, innerHeight);
   cnv.parent('p5Sketch');
-  inc = PI / 10;
+  inc = PI / 50;// here you can change the speed
 }
 
 function draw() {
@@ -21,6 +21,7 @@ function draw() {
   vertices = document.getElementById('vertices').value;
   for (let i = 1; i < loop; i++) {
     waveFreq = map(i, 0, loop, 0, TWO_PI * period);
+    // this part need to be explained really well!!!
     drawPolygon(vertices, i, width / 2, height / 2 + (i * sin(wave + waveFreq) * waveHeight));// change here the posY position to make the circles mive on y axis
   }
   wave += inc;
@@ -28,7 +29,7 @@ function draw() {
 
 function drawPolygon(vertices, num, posX, posY) {
   // how to draw a circle using sine and cosine
-  beginShape();
+  beginShape(POINTS);
   for (let i = 0; i < vertices; i++) {
     let angle = map(i, 0, vertices, 0, TWO_PI);
     let x = num * radius * cos(angle) + posX;
